fba9e11066d44a9a0d1df99629d0acb0fa62b725
OneGrasp Developer <developer@onegrasp.com>
2026-06-26 18:44:19 +0530

    speaker update

--- src/utils/payment.ts
+++ src/utils/payment.ts
@@ -2,7 +2,7 @@
 export const RAZORPAY_CONFIG = {
   KEY_ID: 'rzp_live_T4fgWI2uotntDG',
   KEY_SECRET: 'KQWPCNCW2sg63Xi4VMSqZXmN',
-  AMOUNT: 1 * 100, // Amount in paise (299 INR)
+  AMOUNT: 299 * 100, // Amount in paise (299 INR)
 };
 
 // WhatsApp configuration
